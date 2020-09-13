16e4e60df3b58f30af85590e459df11d5a2f3d1b
may7941 <7941gil@gmail.com>
2020-09-13 12:12:51 +0900

    Create Components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Customer from './components/Customer'
 import './App.css';
 
-function App() {
-  return (
-    <div className="gray-background">
-        <img src={logo} alt="logo" />
-        <h2>Let's develop management system!</h2>
-    </div>
-  );
+const customer = {
+  'name': '길태우',
+  'birthday': '941220',
+  'gender': '남자',
+  'job': '무직'
+}
+
+class App extends Component {
+  render() {
+    return (
+      <Customer
+        name={customer.name}
+        birthday={customer.birthday}
+        gender={customer.gender}
+        job={customer.job}
+      />
+    );
+  }
 }
 
 export default App;
